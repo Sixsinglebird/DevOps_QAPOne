7f65916eaf457d79effc94ce6df7d847a2fdbf9e
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-11 09:25:13 -0230

    maven

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -1,20 +1,21 @@
 package com.keyin;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
-import java.util.Objects;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class SuggestionEngineTest {
     SuggestionEngine suggestionEngine = new SuggestionEngine();
-    @org.junit.jupiter.api.Test
+
+    @Test
     public void loadDictionaryData() throws URISyntaxException, IOException {
         suggestionEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
     }
 
-    @org.junit.jupiter.api.Test
+   @Test
     public void generateSuggestions() throws IOException, URISyntaxException {
         suggestionEngine.loadDictionaryData(Paths.get( ClassLoader.getSystemResource("words.txt").toURI()));
         Assertions.assertTrue(suggestionEngine.generateSuggestions("hellp").contains("sdafasdfgasdfg"));
